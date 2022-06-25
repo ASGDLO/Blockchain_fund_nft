2425162e111bebc4f525ff060175c2e1dcd719a4
Ethan <32297705+ASGDLO@users.noreply.github.com>
2022-06-25 01:51:58 -0400

    add

---
+++ nft/types/nft.ts
@@ -0,0 +1,13 @@
+export type Trait = "attack" | "health" | "speed";
+
+export type NftAttribute = {
+    trait_type : Trait;
+    value: string;
+}
+
+export type NftMeta = {
+    name : string;
+    description : string;
+    image:string;
+    attributes: NftAttribute[];
+}

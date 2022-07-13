aa81e3cb42236ff9301a2a2dadd50404ffcf9870
Ethan <32297705+ASGDLO@users.noreply.github.com>
2022-07-13 22:09:01 +0900

    +

--- jwt33.js
+++
@@ -1,7 +0,0 @@
-const CUBE_API_SECRET = process.env.NEXT_PUBLIC_CUBEJS_TOKEN;
-const jwt = require('jsonwebtoken');
-export const cubejsToken = jwt.sign(
-	{}, CUBE_API_SECRET, { expiresIn: '30d' }
-);
-
-// console.log(cubejsToken);
